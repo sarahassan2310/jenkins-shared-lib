4d88855e58ac1d59aa04f7b6b8fe91f2a1b46b69
Sarah Hassan <112162414+sarahassan2310@users.noreply.github.com>
2026-05-04 10:48:27 +0200

    Add tools and environment variables to pipeline

--- vars/pipelineTemplate.groovy
+++ vars/pipelineTemplate.groovy
@@ -4,25 +4,121 @@ def call(Map config = [:]) {
 
         agent any
 
+        tools {
+            maven 'maven-iti'
+            jdk 'java17'
+        }
+
+        environment {
+            IMAGE_NAME = "${config.imageName}"
+            IMAGE_TAG  = "${config.imageTag ?: 'latest'}"
+            APP_PORT   = "${config.port ?: '8080'}"
+        }
+
         stages {
 
-            stage('Clone Repository') {
+            // 1 - CLONE
+            stage('Clone') {
                 steps {
-                    script {
+                    git branch: "${config.branch ?: 'main'}",
+                        url: config.repo,
+                        credentialsId: 'github-token'
+                }
+            }
 
-                        def repo = config.gitRepo
-                        def branch = config.gitBranch ?: 'main'
+            // 2 - COMPILE
+            stage('Compile') {
+                steps {
+                    sh "mvn clean compile"
+                }
+            }
 
-                        echo "Cloning repository..."
-                        echo "Repo: ${repo}"
-                        echo "Branch: ${branch}"
+            // 3 - TEST
+            stage('Test') {
+                steps {
+                    sh "mvn test -Dspring.profiles.active=test"
+                }
+            }
 
-                        git branch: branch,
-                            url: repo
+            // 4 - PACKAGE
+            stage('Package') {
+                steps {
+                    sh "mvn clean package -DskipTests"
+                }
+            }
+
+            // 5 - DOCKER LOGIN
+            stage('Docker Login') {
+                steps {
+
+                    withCredentials([usernamePassword(
+                        credentialsId: 'dockerhub',
+                        usernameVariable: 'DOCKER_USER',
+                        passwordVariable: 'DOCKER_PASS'
+                    )]) {
+
+                        sh '''
+                        echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+                        '''
                     }
                 }
             }
 
+            // 6 - BUILD IMAGE
+            stage('Build Docker Image') {
+                steps {
+
+                    withCredentials([usernamePassword(
+                        credentialsId: 'dockerhub',
+                        usernameVariable: 'DOCKER_USER',
+                        passwordVariable: 'DOCKER_PASS'
+                    )]) {
+
+                        sh '''
+                        docker build -t $DOCKER_USER/${IMAGE_NAME}:${IMAGE_TAG} .
+                        '''
+                    }
+                }
+            }
+
+            // 7 - PUSH IMAGE
+            stage('Push Docker Image') {
+                steps {
+
+                    withCredentials([usernamePassword(
+                        credentialsId: 'dockerhub',
+                        usernameVariable: 'DOCKER_USER',
+                        passwordVariable: 'DOCKER_PASS'
+                    )]) {
+
+                        sh '''
+                        docker push $DOCKER_USER/${IMAGE_NAME}:${IMAGE_TAG}
+                        '''
+                    }
+                }
+            }
+
+            // 8 - DEPLOY
+            stage('Deploy') {
+                steps {
+
+                    withCredentials([usernamePassword(
+                        credentialsId: 'dockerhub',
+                        usernameVariable: 'DOCKER_USER',
+                        passwordVariable: 'DOCKER_PASS'
+                    )]) {
+
+                        sh '''
+                        docker rm -f ${IMAGE_NAME} || true
+
+                        docker run -d \
+                        --name ${IMAGE_NAME} \
+                        -p ${APP_PORT}:8080 \
+                        $DOCKER_USER/${IMAGE_NAME}:${IMAGE_TAG}
+                        '''
+                    }
+                }
+            }
         }
     }
 }
